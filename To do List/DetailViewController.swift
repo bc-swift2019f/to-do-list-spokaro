0405fe406cb55ad122e9caa2aca6cf0eaec56242
andres <spokaro21@gmail.com>
2019-09-23 03:02:16 -0400

    5.6 Complete

--- To do List/DetailViewController.swift
+++ To do List/DetailViewController.swift
@@ -12,6 +12,11 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var toDoField: UITextField!
     var toDoItem: String?
+    var toDoNoteItem: String?
+    
+    @IBOutlet weak var saveBarBUtton: UIBarButtonItem!
+    
+    @IBOutlet weak var toDoNoteView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +25,30 @@ class DetailViewController: UIViewController {
             toDoField.text = toDoItem
             
         }
+        if let toDoNoteItem = toDoNoteItem {
+            toDoNoteView.text = toDoNoteItem
+            
+        }
+        enableDisableSaveButton()
+        toDoField.becomeFirstResponder()
+    }
+    func enableDisableSaveButton() {
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0 {
+            saveBarBUtton.isEnabled = true
+        }else{
+            saveBarBUtton.isEnabled = false
+        }
+    }
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+       enableDisableSaveButton()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
+            toDoNoteItem = toDoNoteView.text
+            
         }
     }
     
